de718c62759fc2f783676e3e2d5f56ad04eae98a
GKB <karthicbalaji@gmail.com>
2024-03-26 17:59:29 +0530

    Uploading source code

---
+++ src/com/ilayaraja/album/songs/IRSongs.java
@@ -0,0 +1,42 @@
+package com.ilayaraja.album.songs;
+
+public class IRSongs {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        float floatVal = 10;
+        double doubleVal = 1.0;
+        String s = new String("MyName");
+
+        //This is how object for Integer is created till JDK6
+        Integer intOldWay = new Integer(10);
+        //System.out.print(intOldWay.intValue());
+
+        //Autoboxed from Object to primitive type
+        int autoBoxValue = intOldWay;
+        //primitive casting
+        double autoBoxPrimDoubleValue = intOldWay;
+        System.out.println(autoBoxPrimDoubleValue);
+
+        //Narrowing
+//        int narrowedDouble = autoBoxPrimDoubleValue;
+
+        //Object based AutoBoxing
+        Double autoBoxDoubleObj = Double.valueOf(intOldWay);
+        //System.out.println(autoBoxDoubleObj);
+
+        //Autoboxed from Object to primitive type
+        Integer autoBoxIntObj = autoBoxValue;
+        //System.out.println(autoBoxIntObj.byteValue());
+
+        //int result = squreRoot(floatVal);
+
+        //System.out.println(result);
+    }
+
+    public float pitch(Float i) {
+        /*if (i instanceof Integer){
+            i = i+10;
+        }*/
+        return i*i;
+    }
+}
---
+++ src/com/karthic/MySongs.java
@@ -0,0 +1,10 @@
+package com.karthic;
+
+import com.ilayaraja.album.songs.IRSongs;
+
+public class MySongs {
+    public static void main(String[] args) {
+        IRSongs irSongsObj = new IRSongs();
+        irSongsObj.pitch(2f);
+    }
+}
